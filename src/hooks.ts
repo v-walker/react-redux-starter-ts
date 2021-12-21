3003c3f8518bf9e2fa97ffdea9cb4c17e43e506a
Victoria Walker <vmsmith2014@gmail.com>
2021-12-21 09:58:51 -0500

    clean-up and organization; local storage not needed at this time

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,3 +4,12 @@ import type { RootState, AppDispatch } from './index';
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+
+// IMPORTANT!
+// in components, import the following:
+// import { useAppDispatch, useAppSelector } from '../hooks';
+
+// inside of component function, example of use:
+// const dispatch = useAppDispatch();
+// const count = useAppSelector(state => state.sampleCRD.count);
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { compose } from 'redux';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
-// import sampleReducer from './reducers/sampleReducer'
 
 import './index.css';
 import App from './App';
@@ -18,43 +17,11 @@ declare global {
   }
 }
 
-const saveToLocalStorage = (reduxGlobalState: any) => {
-  // serialization = converting js object to a string
-  try {
-    const serializedState = JSON.stringify(reduxGlobalState);
-    localStorage.setItem('state', serializedState);
-
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-// const loadFromLocalStorage = () => {
-//   // deserialization --> converting string to an object
-//   const state = localStorage.getItem('state');
-
-//   if (state == null) {
-//     return undefined;
-//   } else {
-//     return JSON.parse(state);
-//   }
-// }
-
-// const persistedState = loadFromLocalStorage();
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-// const store = createStore(rootReducer, persistedState, composeEnhancers());
 const store = configureStore({
       reducer: rootReducer,
       middleware: getDefaultMiddleware => getDefaultMiddleware()
 });
 
-store.subscribe(() => {
-  
-  // happens every time there is a change to the global state
-  saveToLocalStorage(store.getState())
-
-})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -76,3 +43,5 @@ ReactDOM.render(
 export type RootState = ReturnType<typeof store.getState>;
 // // Inferred type: {}
 export type AppDispatch = typeof store.dispatch;
+
+// ref ./hooks.ts for use and import to components
